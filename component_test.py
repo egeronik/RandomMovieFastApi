1b36a293c78a3428e153ca0e5e5bb77246f45654
e.grebennik@dev.group-ib.com <eshik2002@gmail.com>
2023-12-12 18:24:37 +0300

    Updated tests

--- component_test.py
+++ component_test.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from uuid import UUID, uuid4
 from datetime import datetime
+import json
 
 base_url = 'http://localhost:80/'
 
@@ -15,8 +16,8 @@ def test_root_get():
 def test_list_get():
     res = requests.get(base_url+'list/?q=2754')
     print(res)
-    return
-    res = res[0]
+    res = json.loads(res.text)[0]
+    print(res)
     pytest.assume(res['kinopoiskId']==2754)
     pytest.assume(res['nameRu']=='Сто и одна ночь Симона Синема')
     pytest.assume(res['posterUrl']=="https://kinopoiskapiunofficial.tech/images/posters/kp/2754.jpg")
